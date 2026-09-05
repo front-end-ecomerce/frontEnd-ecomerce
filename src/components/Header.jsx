27c473e683a88bb27fbc8702f11f217d106814c8
hani <aragsanahmed06@gmail.com>
2026-09-05 19:18:56 +0300

    header update

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header({ cartCount }) {
+  return (
+    <header>
+      <div>
+        <div>N</div>
+        <span>NORDLY</span>
+      </div>
+
+      
+    </header>
+  )
+}
+
+export default Header;
