15592239d9df85c9a0473f6a041ab19dbc2d0f22
Maashka24 <abdirashidh2021@gmail.com>
2026-09-18 13:25:30 +0200

    Routes and Route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero"
 import Shopping from "./components/Shopping";
 import Footer from "./components/Footer"
 import { useState } from "react";
+import {Routes, Route} from "react-router-dom";
 
 function App() {
   const [cartCount, setCartCount] = useState(0);
@@ -15,9 +16,26 @@ function App() {
   return (
         
       <div className="">
-           <Header  CartCount={cartCount} />
-        <Hero />
-        <Shopping AddToCart={AddtoCart} />
+
+           <Header  cartCount={cartCount} />
+           
+
+           <Routes>
+            <Route path="/" element={
+              <>
+              <Hero />
+              
+            <Shopping AddtoCart={AddtoCart} />
+              </>
+
+            } />
+
+            <Route path="shop" element={<Shopping AddtoCart={AddtoCart} />}/>
+       
+
+            <Route path="about" element={<about/>}/>
+
+        </Routes>
         <Footer/>
       </div>
     
